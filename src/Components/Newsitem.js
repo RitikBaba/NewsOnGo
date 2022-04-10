9bce971ec524e581cd7646318ec107989b354fab
ritik <ritikbaba28@gmail.com>
2022-04-10 22:16:28 +0530

    github push

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -1,22 +1,48 @@
-import { getByTitle } from '@testing-library/react'
-import React, { Component } from 'react'
+import { getByTitle } from "@testing-library/react";
+import React, { Component } from "react";
 
 export class Newsitem extends Component {
   render() {
-      let {title,description,imageUrl,newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
-      <div className='my-3'>
-           <div className="card" >
-        <img src={imageUrl?imageUrl:"https://s.yimg.com/ny/api/res/1.2/JvJ_WqsNC8yaGY.hJwh0Kw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2022-03/688d0690-b40d-11ec-afe7-c65cbb21cf8e"} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{title}...</h5>
-          <p className="card-text">{description}...</p>
-          <a href={newsUrl} target="_blank" className="btn btn-outline-dark d-flex justify-content-center">Read More</a>
+      <div className="my-3">
+        <div className="card">
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://s.yimg.com/ny/api/res/1.2/JvJ_WqsNC8yaGY.hJwh0Kw--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2022-03/688d0690-b40d-11ec-afe7-c65cbb21cf8e"
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">
+              {title}...
+              <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>
+                {source}
+                <span class="visually-hidden">unread messages</span>
+              </span>
+            </h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {author ? author : "Unknown"} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-outline-dark d-flex justify-content-center"
+            >
+              Read More
+            </a>
+          </div>
         </div>
       </div>
-      </div>
-    )
+    );
   }
 }
 
-export default Newsitem
+export default Newsitem;
